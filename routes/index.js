aa6a57c1f4649f46a76b5434b18083146aaffe8e
Artur Szalankiewicz <as@ambsoft.pl>
2016-03-24 16:13:16 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var cool = require('cool-ascii-faces');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/cool', function(request, response) {
+  response.send(cool());
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resourcdddddddddde');
+});
+
+router.get('/test', function(req, res, next) {
+  res.send('respond with a test');
 });
 
 module.exports = router;
